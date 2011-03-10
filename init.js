5160b7789c1f97609b51963efe24914ad4516d20
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2011-03-10 21:22:09 +0700

    set default download directories based on file extension

--- init.js
+++ init.js
@@ -205,6 +205,47 @@ clicks_in_new_buffer_target = OPEN_NEW_BUFFER_BACKGROUND;
 add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
 add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 
+// Download
+
+function suggest_save_path_from_file_name(file_name, buffer) {
+    var cwd = with_current_buffer(buffer, function (I) I.local.cwd);
+    var file = cwd.clone();
+    for (let re in replace_map) {
+        if (file_name.match(re)) {
+            if (replace_map[re].path) {
+                file = make_file(replace_map[re].path);
+            }
+            // file_name = replace_map[re].transformer(file_name);
+        }
+    }
+    file.append(file_name);
+    return file.path;
+}
+
+// Default location by type
+let (d = get_home_directory()) {
+    d.append("Downloads");
+    // TODO: Windows?
+    var replace_map = {
+        ".": {
+            "path": d.path,
+            // "transformer": function(x) x
+        },
+        "\.(html|pdf|ps)$": {
+            "path": d.path + "/Documents/",
+        },
+        "\.(dmg|zip)$": {
+            "path": d.path + "/Documents/",
+        },
+        "\.(mp3)$": {
+            "path": d.path + "/Music/",
+        },
+        "\.(mov|mp4|avi)$": {
+            "path": d.path + "/Video/",
+        }
+    }
+}
+
 // Other personal stuffs
 
 // From technomancy
