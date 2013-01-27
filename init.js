da695a35850deadaaf242ef5032bd0bd04ec557e
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-01-27 21:54:17 +0700

    code style fixes

--- init.js
+++ init.js
@@ -164,24 +164,24 @@ interactive("readability_arc90",
                 _readability_readConvertLinksToFootnotes.text = 'var readConvertLinksToFootnotes = ' + readConvertLinksToFootnotes + ';';
                 document.getElementsByTagName('head')[0].appendChild(_readability_readConvertLinksToFootnotes);
 
-                var _readability_script = document.createElement('script')
-                _readability_script.type='text/javascript'
-                _readability_script.src='http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random())
-                document.documentElement.appendChild(_readability_script)
+                var _readability_script = document.createElement('script');
+                _readability_script.type='text/javascript';
+                _readability_script.src='http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random());
+                document.documentElement.appendChild(_readability_script);
 
-                var _readability_css = document.createElement('link')
-                _readability_css.rel = 'stylesheet'
-                _readability_css.href = 'http://lab.arc90.com/experiments/readability/css/readability.css'
-                _readability_css.type = 'text/css'
-                _readability_css.media = 'all'
-                document.documentElement.appendChild(_readability_css)
+                var _readability_css = document.createElement('link');
+                _readability_css.rel = 'stylesheet';
+                _readability_css.href = 'http://lab.arc90.com/experiments/readability/css/readability.css';
+                _readability_css.type = 'text/css';
+                _readability_css.media = 'all';
+                document.documentElement.appendChild(_readability_css);
 
-                var _readability_print_css = document.createElement('link')
-                _readability_print_css.rel = 'stylesheet'
-                _readability_print_css.href = 'http://lab.arc90.com/experiments/readability/css/readability-print.css'
-                _readability_print_css.media = 'print'
-                _readability_print_css.type = 'text/css'
-                document.getElementsByTagName('head')[0].appendChild(_readability_print_css)
+                var _readability_print_css = document.createElement('link');
+                _readability_print_css.rel = 'stylesheet';
+                _readability_print_css.href = 'http://lab.arc90.com/experiments/readability/css/readability-print.css';
+                _readability_print_css.media = 'print';
+                _readability_print_css.type = 'text/css';
+                document.getElementsByTagName('head')[0].appendChild(_readability_print_css);
             });
 
 
