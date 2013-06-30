c4a0473af699ae682071b1dad7aaeae5f0cae614
Nguyễn Tuấn Anh <ubolonton@ubolonton-laptop.(none)>
2013-06-30 21:42:16 +0700

    add convenient util functions for debugging

---
+++ config/ublt-util.js
@@ -0,0 +1,55 @@
+
+function m_class(el) {
+  var cl = el.getAttribute("class");
+  cl = cl ? cl.split(" ")[0] : "";
+  return cl ? "." + cl : "";
+}
+
+function m_id(el) {
+  var id = el.getAttribute("id");
+  return id ? "#" + id : "";
+}
+
+function css_selector(el) {
+  var e = el;
+  var sel = [];
+  while (e && e.tagName != "BODY") {
+    sel.push(e.tagName.toLowerCase() + m_id(e) + m_class(e));
+    e = e.parentElement;
+  }
+  return sel.reverse().join(" ");
+}
+
+function pp(obj, all) {
+  var l = [];
+  for (var p in obj)
+    if (all || Object.hasOwnProperty.call(obj, p))
+      l.push(p + ": " + obj[p]);
+  return l.join("\n");
+}
+
+function pl(obj, all) {
+  var l = [];
+  for (var p in obj)
+    if (all || Object.hasOwnProperty.call(obj, p))
+      l.push(p);
+  return l.join("\n");
+}
+
+function pc(obj, all) {
+  var count = 0;
+  for (var p in obj)
+    if (all || Object.hasOwnProperty.call(obj, p))
+      count ++;
+  return count;
+}
+
+function contentW() {
+  return repl_context().window.buffers.current.browser.contentWindow;
+}
+
+function w() {
+    return repl_context().window;
+}
+
+provide("ublt-util");
