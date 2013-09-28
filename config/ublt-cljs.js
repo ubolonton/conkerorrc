e47400bfce6e9d64e0a194376931f094f437dd65
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-09-28 13:01:29 +0700

    refactor ublt-cljs (most of it actually deals with goog though)

--- config/ublt-cljs.js
+++ config/ublt-cljs.js
@@ -1,7 +1,10 @@
 var {classes: Cc, Constructor: CC, interfaces: Ci, utils: Cu,
      results: Cr, manager: Cm } = Components;
 
+// Note that goog.dependencies_.written in our context means "loaded" instead
+
 // EXPORTED_SYMBOLS problem, so Cu.import is a no no
+
 
 // TODO: Is this the best way to load script?
 function loadScript(url, context) {
@@ -11,7 +14,7 @@ function loadScript(url, context) {
   Services.scriptloader.loadSubScript(url, context);
 }
 
-function makeEnv(baseURL) {
+function goog_make_env(baseURL) {
   // TODO: Do we really need a system principal?
   var principal = CC("@mozilla.org/systemprincipal;1", "nsIPrincipal")();
   // TODO: And do we really need a sandbox?
@@ -24,25 +27,57 @@ function makeEnv(baseURL) {
   });
 }
 
-function googURL(module, env) {
+
+function goog_url(module, env) {
   if (!env.CLOSURE_BASE_PATH) {
     throw Error("CLOSURE_BASE_PATH not set");
   }
   return env.CLOSURE_BASE_PATH + module + ".js";
 }
 
-function setUp(env) {
+
+function goog_set_up(env) {
   // src is a URL
   env.CLOSURE_IMPORT_SCRIPT = function(src) {
     // TODO: How do we handle possible errors here?
     loadScript(src, env);
     return true;
   };
-  loadScript(googURL("base", env), env);
+  loadScript(goog_url("base", env), env);
   // TODO: Maybe this should be optional?
-  loadScript(googURL("deps", env), env);
+  loadScript(goog_url("deps", env), env);
 }
 
-var cljs_env = makeEnv("file:///home/ubolonton/Programming/Clojure/lib/clojurescript/closure/library/closure/goog/");
+
+// deps is a list of [file, provides, requires]
+// additions is additional ns => module mapping for modules loaded outside
+function goog_new_env(baseURL, deps, additions) {
+  var cljs = goog_make_env(baseURL);
+  goog_set_up(cljs);
+  deps.forEach(function(dep) {
+    let [file, provides, requires] = dep;
+    cljs.goog.addDependency(file, provides, requires);
+  });
+  for (var ns in additions) {
+    let module = additions[ns];
+    cljs.goog.provide(ns);
+    cljs.goog[ns] = module;
+  }
+  return cljs;
+}
+
+
+var cljs = goog_new_env(
+  "file:///home/ubolonton/Programming/Clojure/lib/clojurescript/closure/library/closure/goog/",
+  // XXX: Google Closure doesn't seem to support a list of search paths
+  // For (require '[cljs.core])
+  [["../../../../out/cljs/core.js", ["cljs.core"],
+    ["goog.string", "goog.string.StringBuffer",
+     "goog.object", "goog.array"]]], {
+    "repl": repl,
+    // For (require '[conkeror])
+    "conkeror": Cc["@conkeror.mozdev.org/application;1"].getService().wrappedJSObject
+  });
+
 
 provide("ublt-cljs");
