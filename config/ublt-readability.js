d2987b798cb5dc2ab64423bd12936b7269444eaf
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-09-28 17:14:01 +0700

    readability needs a token now; add readability-read-later

--- config/ublt-readability.js
+++ config/ublt-readability.js
@@ -1,50 +1,36 @@
-
 // Integration with readability.com
 
-interactive("readability-arc90",
-            "Readability is a simple tool that makes reading on the web more enjoyable by removing the clutter around what you are reading",
-            function readability_arc90(I) {
-                var document = I.window.buffers.current.document;
+var readability_token = 'u3UkrgRbELzG4mM5CGBEnu2qLMVC8JNnxXd76Mtc';
 
-                var readConvertLinksToFootnotes = false;
-                var readStyle = 'style-newspaper';
-                var readSize = 'size-medium';
-                var readMargin = 'margin-wide';
+function readability_read_now(I) {
+    var window = I.window.buffers.current;
+    var document = window.document;
+    ((function(){
+        window.baseUrl = 'http://www.readability.com';
+        window.readabilityToken = readability_token;
+        var s = document.createElement('script');
+        s.setAttribute('type', 'text/javascript');
+        s.setAttribute('charset', 'UTF-8');
+        s.setAttribute('src', window.baseUrl + '/bookmarklet/read.js');
+        document.documentElement.appendChild(s);
+    })());
+}
 
-                var _readability_readStyle = document.createElement('SCRIPT');
-                _readability_readStyle.text = 'var readStyle = \'' + readStyle + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readStyle);
+function readability_read_later(I) {
+    var window = I.window.buffers.current;
+    var document = window.document;
+    ((function(){
+        window.baseUrl = 'http://www.readability.com';
+        window.readabilityToken = readability_token;
+        var s = document.createElement('script');
+        s.setAttribute('type', 'text/javascript');
+        s.setAttribute('charset', 'UTF-8');
+        s.setAttribute('src', window.baseUrl + '/bookmarklet/save.js');
+        document.documentElement.appendChild(s);
+    })());
+}
 
-                var _readability_readSize = document.createElement('SCRIPT');
-                _readability_readSize.text = 'var readSize = \'' + readSize + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readSize);
-
-                var _readability_readMargin = document.createElement('SCRIPT');
-                _readability_readMargin.text = 'var readMargin = \'' + readMargin + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readMargin);
-
-                var _readability_readConvertLinksToFootnotes = document.createElement('SCRIPT');
-                _readability_readConvertLinksToFootnotes.text = 'var readConvertLinksToFootnotes = ' + readConvertLinksToFootnotes + ';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readConvertLinksToFootnotes);
-
-                var _readability_script = document.createElement('script');
-                _readability_script.type='text/javascript';
-                _readability_script.src='http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random());
-                document.documentElement.appendChild(_readability_script);
-
-                var _readability_css = document.createElement('link');
-                _readability_css.rel = 'stylesheet';
-                _readability_css.href = 'http://lab.arc90.com/experiments/readability/css/readability.css';
-                _readability_css.type = 'text/css';
-                _readability_css.media = 'all';
-                document.documentElement.appendChild(_readability_css);
-
-                var _readability_print_css = document.createElement('link');
-                _readability_print_css.rel = 'stylesheet';
-                _readability_print_css.href = 'http://lab.arc90.com/experiments/readability/css/readability-print.css';
-                _readability_print_css.media = 'print';
-                _readability_print_css.type = 'text/css';
-                document.getElementsByTagName('head')[0].appendChild(_readability_print_css);
-            });
+interactive("readability-read-now", null, readability_read_now);
+interactive("readability-read-later", null, readability_read_later);
 
 provide("ublt-readability");
