01e328f6d7ef7522895089798bd22036fa9652f9
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2015-03-25 00:37:12 +0700

    switch to new Grooveshark

--- config/ublt-launchers.js
+++ config/ublt-launchers.js
@@ -53,7 +53,7 @@ ublt.ns("ublt.launcher", {
 // Use xbindkeys to assign keys to these
 ublt.launcher.simple("GMail", "https://mail.google.com");
 ublt.launcher.simple("Facebook", "https://www.facebook.com");
-ublt.launcher.simple("Grooveshark", "http://grooveshark.com/#!/ubolonton/collection");
+ublt.launcher.simple("Grooveshark", "http://preview.grooveshark.com/#!/ubolonton/collection");
 ublt.launcher.simple("Prismatic", "http://getprismatic.com");
 ublt.launcher.define("Coursera", "https://www.coursera.org", function(b) {
   return make_uri(b.display_uri_string).hostPort.search("coursera.org") > -1;
